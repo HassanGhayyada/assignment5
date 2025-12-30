2ddcdc6433f3734c9041169014e0f3ccb9f1d508
Hassan Ghayyada <7assan.gghassan@gmail.com>
2025-12-30 20:44:44 +0300

    added app component with state events conditional rendering and i removed the unused index.css from the main file

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ProductList from './components/ProductList'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
+    const[showProducts,setShowProducts] = useState(true);
+    const[name,setName]= useState("");
+    
+    return(
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+        <h1>React Product Dashboard</h1>
 
-export default App
+        <button onClick={() => setShowProducts(!showProducts)}>
+          {showProducts ? "Hide products" : "Show Products"}
+        </button>
+
+        <div>
+          <input
+          type='text'
+          value={name}
+          onChange={(e) => setNam(e.target.value)}
+          placeholder='Enter Your Name'
+          />
+        </div>
+
+        <p>Hello, {name}</p>
+
+        {showProducts && < ProductList />}
+      </div>
+    );
+}
+export default App;
