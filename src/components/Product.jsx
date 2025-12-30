8cdfc3c899d4e8f3f0e65a3b0b94322003562a33
Hassan Ghayyada <7assan.gghassan@gmail.com>
2025-12-30 19:33:50 +0300

    added the product component using props

---
+++ src/components/Product.jsx
@@ -0,0 +1,10 @@
+import Card from "./Card";
+function Product({title,price}){
+    return(
+        <Card>
+            <h3>{title}</h3>
+            <p>Price: ${price}</p>
+        </Card>
+    );
+}
+export default Product;
